1854eece114852183b096b8d8927b2648ad3c0b4
marcus1688 <148477064+marcus1688@users.noreply.github.com>
2025-12-29 14:59:21 +0800

    Update BankReportDatePicker.vue

--- components/BankReportDatePicker.vue
+++ components/BankReportDatePicker.vue
@@ -22,6 +22,9 @@
 <script setup>
 import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
+import moment from "moment-timezone";
+
+const TIMEZONE = "Asia/Kuala_Lumpur";
 
 const props = defineProps({
   modelValue: {
@@ -47,9 +50,7 @@ const inputValue = ref(props.modelValue);
 
 const minDate = computed(() => {
   if (props.isStartDate && props.otherDate) {
-    const min = new Date(props.otherDate);
-    min.setFullYear(min.getFullYear() - 1);
-    return min;
+    return moment(props.otherDate).tz(TIMEZONE).subtract(1, "year").toDate();
   } else if (!props.isStartDate && props.otherDate) {
     return props.otherDate;
   }
@@ -57,13 +58,13 @@ const minDate = computed(() => {
 });
 
 const maxDate = computed(() => {
-  const today = new Date();
+  const today = moment().tz(TIMEZONE).endOf("day").toDate();
   if (props.isStartDate && props.otherDate) {
-    return new Date(props.otherDate) > today ? today : props.otherDate;
+    const otherMoment = moment(props.otherDate).tz(TIMEZONE);
+    return otherMoment.isAfter(today) ? today : props.otherDate;
   } else if (!props.isStartDate && props.otherDate) {
-    const max = new Date(props.otherDate);
-    max.setFullYear(max.getFullYear() + 1);
-    return max > today ? today : max;
+    const max = moment(props.otherDate).tz(TIMEZONE).add(1, "year").toDate();
+    return moment(max).isAfter(today) ? today : max;
   }
   return today;
 });
@@ -73,8 +74,7 @@ const handleDateChange = (newDate) => {
     emit("update:modelValue", null);
     return;
   }
-
-  emit("update:modelValue", new Date(newDate));
+  emit("update:modelValue", moment(newDate).tz(TIMEZONE).toDate());
 };
 
 watch(
@@ -86,34 +86,6 @@ watch(
 
 const format = (date) => {
   if (!date) return "";
-  const day = date.getDate().toString().padStart(2, "0");
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const year = date.getFullYear();
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  return `${day}/${month}/${year} ${hours}:${minutes}`;
+  return moment(date).tz(TIMEZONE).format("DD/MM/YYYY HH:mm");
 };
 </script>
-
-<style scoped>
-.dp__theme_light {
-  --dp-background-color: #fff;
-  --dp-text-color: #212121;
-  --dp-hover-color: #f3f3f3;
-  --dp-hover-text-color: #212121;
-  --dp-hover-icon-color: #959595;
-  --dp-primary-color: #4f46e5;
-  --dp-primary-text-color: #fff;
-  --dp-secondary-color: #c0c4cc;
-  --dp-border-color: #ddd;
-  --dp-menu-border-color: #ddd;
-  --dp-border-color-hover: #aaaeb7;
-  --dp-disabled-color: #f6f6f6;
-  --dp-scroll-bar-background: #f3f3f3;
-  --dp-scroll-bar-color: #959595;
-  --dp-success-color: #76d275;
-  --dp-success-color-disabled: #a3d9b1;
-  --dp-icon-color: #959595;
-  --dp-danger-color: #ff6f60;
-}
-</style>
